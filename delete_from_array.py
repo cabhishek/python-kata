479c224d149f54ef742149b41b264fb488b05ab1
Abhishek Kapatkar <cabhishek@gmail.com>
2014-12-22 19:14:14 -0500

    Delete number from array without del operator.Sweet

---
+++ delete_from_array.py
@@ -0,0 +1,20 @@
+def delete_from_array(number, array):
+    swap_idx = 0
+
+    for i, _ in enumerate(array):
+        if number == array[i]:
+            # Mark empty by setting 0
+            array[i] = 0
+            # If more than one match then move
+            # all those matched elements along.
+            # so go father back.
+            swap_idx = min(i, swap_idx)
+        else:
+            # Swap
+            array[swap_idx], array[i] = array[i], array[swap_idx]
+            swap_idx += 1
+
+    return array
+
+
+print delete_from_array(3, [5,3,7,11,2,3,13,5,7,3,9,8])
