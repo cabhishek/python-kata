75557fec92a2b06495b6d43f9a23e91c27667b68
Abhishek Kapatkar <cabhishek@gmail.com>
2014-12-26 00:09:05 -0500

    Various ways to loop in python

---
+++ loops.py
@@ -0,0 +1,38 @@
+def loop(intervals):
+
+    print 'Basic pythonic loop'
+    for interval in intervals:
+        print interval
+
+    print 'Basic + Loop index'
+    for i, interval in enumerate(intervals):
+        print i, interval
+
+    print 'Start from index 1'
+    for i in range(1, len(intervals)):
+        print intervals[i]
+
+    print 'Choose index and start position'
+    for i, interval in enumerate(intervals[1:], start=1):
+        print i, interval
+
+    print 'Basic while loop'
+    i = 0
+    while i < len(intervals):
+        print i, intervals[i]
+        i +=1
+
+    print 'Simulate \'while\' with \'for\''
+    for i in range(len(intervals)):
+        print i, intervals[i]
+
+    print 'Basic backward loop'
+    for interval in intervals[::-1]:
+        print interval
+
+    print 'Loop with flexible step interval'
+    for interval in intervals[::2]:
+        print interval
+
+
+loop([[1,3],[2,6],[8,10],[9,18],[19,23]])
