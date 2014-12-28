6b1ef3c64be14a95e6889ba839e7ca73ad3eba04
Abhishek Kapatkar <cabhishek@gmail.com>
2014-12-28 13:50:39 -0500

    Sprial matrix print

---
+++ min_path_sum.py
@@ -0,0 +1,26 @@
+"""
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+Please dont use built in min function.
+"""
+def min_path_sum(triangle):
+    min_sum = 0
+
+    for row in triangle:
+        row_min = row [0]
+
+        for i in range(1, len(row)):
+            if row[i] < row_min:
+                row_min = row[i]
+
+        min_sum += row_min
+
+    return min_sum
+
+triangle = [
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+
+print min_path_sum(triangle)
---
+++ spiral_print.py
@@ -0,0 +1,75 @@
+
+def spiral_traverse(matrix):
+
+    if not len(matrix): return
+
+    top = left = 0
+    bottom = len(matrix)-1
+    right = len(matrix[0]) -1
+
+    dir = 0
+
+    while top <= bottom:
+
+        # go right
+        if dir == 0:
+            # i = left
+
+            for i in range(left, right+1):
+            # while i <= right:
+                yield matrix[top][i]
+                # i += 1
+            top += 1
+
+        # go down
+        elif dir == 1:
+            # j = top
+            for j in range(top, bottom+1):
+
+            # while j <= bottom:
+                yield matrix[j][right]
+                # j += 1
+
+            right -= 1
+
+        # go left
+        elif dir == 2:
+            # k = right
+
+            for k in range(right, left-1, -1):
+            # while k >= left:
+                yield matrix[bottom][k]
+                # k -= 1
+
+            bottom -= 1
+
+        # go up
+        else:
+            # p = bottom
+
+            for p in range(bottom, top-1, -1):
+            # while p >= top:
+                yield matrix[p][left]
+                # p -= 1
+
+            left += 1
+
+        # change direction
+        dir = (dir + 1) % (len(matrix))
+
+matrix = [[2,4,6,8],
+          [5,9,12,16],
+          [2,11,5,9],
+          [3,2,1,8]]
+
+# matrix = [[1,2,3],
+#           [4,5,6],
+#           [7,8,9]]
+
+# matrix = [[1,2],
+#           [4,5],
+#           [7,8]]
+
+# matrix = []
+
+print [data for data in spiral_traverse(matrix)]
