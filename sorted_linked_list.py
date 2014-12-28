e211952a2335ba4878133952659d16b2cad6d608
Abhishek Kapatkar <cabhishek@gmail.com>
2014-12-28 16:40:03 -0500

    sorted linked list

---
+++ sorted_linked_list.py
@@ -0,0 +1,67 @@
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList(object):
+
+    def __init__(self):
+        self.list = None
+
+    def insert(self, data):
+
+        node = Node(data)
+
+        # First node
+        if not self.list:
+            self.list = node
+        else:
+            curr = self.list
+            prev = None
+
+            # Move ahead
+            while curr:
+                if data < curr.data:
+                    prev = curr
+                    curr = curr.next
+                else:
+                    break
+
+            # Append new smallest number
+            # if not curr:
+            #     curr = node
+            #     prev.next = curr
+            if not prev:
+                node.next = self.list
+                self.list = node
+            else:
+                # print 'curr ->', curr
+                node.next = curr
+                prev.next = node
+                prev = node
+
+    def __repr__(self):
+        curr = self.list
+        queue = []
+
+        while curr:
+            queue.append(curr.data)
+            curr = curr.next
+
+        return str(queue)
+
+    def __len__(self):
+        return self.count
+
+list = LinkedList()
+
+list.insert(10)
+list.insert(4)
+list.insert(6)
+list.insert(2)
+list.insert(7)
+list.insert(1)
+
+print list
