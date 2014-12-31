0ba801616c287a605ed53360cfdd7e554e0e69be
Abhishek Kapatkar <cabhishek@gmail.com>
2014-12-31 14:41:46 -0500

    Multiply with reduce and seed value

---
+++ multiple.py
@@ -0,0 +1,28 @@
+"""
+You have an array of integers, and for each index you want to find the product of every integer except the integer at that index.
+For example, given:
+  [1, 7, 3, 4]
+
+your function would return:
+  [84, 12, 28, 21]
+
+by calculating:
+  [7*3*4, 1*3*4, 1*7*4, 1*7*3]
+
+Do not use division in your solution.
+"""
+from operator import mul
+
+def multiply(array):
+
+    products = []
+    previous = 1
+
+    for i in range(0, len(array)):
+        if i: previous *= array[i-1]
+
+        products.append(reduce(mul, array[i+1:], previous))
+
+    return products
+
+print multiply([1, 7, 3, 4])
