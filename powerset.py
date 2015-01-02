da2bd9154d70e7b0274329279724e946202b5e78
Abhishek Kapatkar <cabhishek@gmail.com>
2015-01-01 22:28:52 -0500

    fix error in powerset

--- powerset.py
+++ powerset.py
@@ -1,18 +1,32 @@
 """ Subsets possible from a list
 """
-def power_set(numbers):
-    sets = [[]]
+#def power_set(numbers):
+    #sets = [[]]
 
-    if not numbers: return sets
+    #if not numbers: return sets
 
-    sets += [numbers]
+    #sets += [numbers]
 
-    for i, number in enumerate(numbers):
-        sets.append([number])
+    #for i, number in enumerate(numbers):
+        #sets.append([number])
 
-        for x in numbers[i+1:]:
-            sets.append([number] + [x])
+        #for x in numbers[i+1:]:
+            #sets.append([number] + [x])
 
-    return sets
+    #return sets
 
-print power_set([1,2,3])
+def powerset(lst):
+    # the power set of the empty set has one element, the empty set
+    result = [[]]
+    for x in lst:
+        # for every additional element in our set
+        # the power set consists of the subsets that don't
+        # contain this element (just take the previous power set)
+        # plus the subsets that do contain the element (use list
+        # comprehension to add [x] onto everything in the
+        # previous power set)
+        result.extend([subset + [x] for subset in result])
+    return result
+
+print powerset([1,2,3,4])
+
