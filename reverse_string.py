24513340830fd36ed68bc683b8766cb7b1a7968c
Abhishek Kapatkar <cabhishek@gmail.com>
2015-01-27 20:32:44 -0500

    crappy reverse words

--- reverse_string.py
+++ reverse_string.py
@@ -19,5 +19,37 @@ def reverse_2(string):
 
     return " ".join(array)
 
+def reverse_3(string):
+
+    def reverse(string):
+        start = 0
+        end   = len(string)-1
+        string = list(string)
+
+        while end > start:
+            string[start], string[end] = string[end], string[start]
+
+            start += 1
+            end   -= 1
+
+        return "".join(string)
+
+    string = list(reverse(string))
+    i = 0
+    reversed = ""
+
+    for j in range(len(string)):
+        if string[j] == ' ':
+
+            if not reversed:
+                reversed = reverse(string[i:j])
+            else:
+                reversed = reversed + " " + reverse(string[i:j])
+
+            i = j + 1
+
+    return reversed + " " +  reverse(string[i:])
+
 print reverse("Hello World")
 print reverse_2("Hello from Mars")
+print reverse_3("Hello world everyone")
