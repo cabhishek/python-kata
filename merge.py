2f2727363d994cbb1b0b02d1eab68fccf7152be5
Abhishek Kapatkar <cabhishek@gmail.com>
2015-01-31 08:55:54 -0500

    Simple merge after pivot element

--- merge.py
+++ merge.py
@@ -24,3 +24,46 @@ B = [4,11,18]
 # B = [0,1,2]
 
 print merge(A, B)
+
+def arrange(A, k):
+    # All elments less than A[K] before and greater after A[K]
+    array = [None] * len(A)
+    i = 0
+    j = len(A)-1
+
+    start = 0
+    end = len(A)-1
+
+    while i < k:
+
+        if A[i] >= A[k]:
+            array[end] = A[i]
+
+            end -=1
+            i +=1
+        else:
+            array[start] = A[i]
+            start += 1
+            i += 1
+
+    while j > k:
+
+        if A[j] <= A[k]:
+            array[start] = A[j]
+            start +=1
+            j -=1
+        else:
+            array[end] = A[j]
+            end -=1
+            j -=1
+
+    array[end] = A[k]
+
+    return array
+
+
+
+# A = [4,8,2,12,18,4,1]
+A = [10,12,13,14,4,5,6,7]
+
+print arrange(A,2)
