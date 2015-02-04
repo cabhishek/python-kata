a6e86f9ed43770d45cef72737d0a6b0b12cc55bd
Abhishek Kapatkar <cabhishek@gmail.com>
2015-02-03 20:46:51 -0500

    sort stack without extra space

--- sort_stack.py
+++ sort_stack.py
@@ -26,3 +26,36 @@ def sort_stack(stack):
 
 
 print sort_stack([7, 73, 65, 23, 8, 77, 1, 24])
+
+
+def sort(A):
+    # Sort stack without additional space
+    # basically stack + recursion i.e more stacks. FUN
+    def insert(A, num):
+        if not A:
+            A.append(num)
+        else:
+            if num < A[-1]:
+                last_popped = A.pop()
+                insert(A, num)
+                insert(A, last_popped)
+            else:
+                A.append(num)
+
+        return A
+
+    if not A: return A
+
+    num = A.pop()
+    sort(A)
+    insert(A, num)
+
+    return A
+
+# A = [4,5,3,1,2]
+# A = [1,2,3,4,5]
+
+A = [1,2,3,6,5,4]
+
+print sort(A)
+
